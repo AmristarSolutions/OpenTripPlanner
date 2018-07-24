ef0b6f789923f38b99de235ba28ef86704150f5d
Viet Nguyen <viet.ng304@gmail.com>
2018-07-24 16:26:03 +0800

    UPDATE: TRANS-44 update start script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+otpHome="/home/transperth"
 if [ "$1" != "" ]; then
         log=$1
 else
@@ -6,9 +7,9 @@ else
 fi
 
 if [ "$2" == "build" ]; then
-        build="--build /home/amristar/otp/data/graphs/perth"
+        build="--build $otpHome/otp/data/graphs/perth"
 else
         build=""
 fi
 
-java -Xmx8G -jar ~/otp/OpenTripPlanner/target/otp-1.3.0-SNAPSHOT-shaded.jar --basePath ~/otp/data $build --insecure --server --preFlight --autoScan > $log
+java -Xmx8G -jar $otpHome/otp/OpenTripPlanner/target/otp-1.3.0-SNAPSHOT-shaded.jar --basePath $otpHome/otp/data $build --insecure --server --preFlight --autoScan > $log
